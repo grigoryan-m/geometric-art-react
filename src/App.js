a27aff6dfaee6f75ff2f2dda413ab310f05ce39e
grigoryan.m <waldm4rk@gmail.com>
2023-09-22 11:32:41 +0400

    adding random colors

--- src/App.js
+++ src/App.js
@@ -7,24 +7,38 @@ function App() {
   
   useEffect(() => {
     const cells = document.getElementsByClassName("cell");
-    let randomClass;
+    const generateButton = document.getElementById("generateButton");
 
-    for(let cell of cells){
-      randomClass = Math.random() * 4;
-      randomClass = Math.round(randomClass);
-      cell.classList = "cell";
-      if(randomClass === 0){
-        cell.classList.add("tl");
-      }else if(randomClass === 1){
-        cell.classList.add("tr");
-      }else if(randomClass === 2){
-        cell.classList.add("bl");
-      }else if(randomClass === 3){
-        cell.classList.add("br");
-      }else if(randomClass === 4){
-        cell.classList.add("all");
+    const generate = function(){
+      for(let cell of cells){
+        let randomClass = Math.random() * 4;
+        randomClass = Math.round(randomClass);
+        cell.classList = "cell";
+        if(randomClass === 0){
+          cell.classList.add("tl");
+        }else if(randomClass === 1){
+          cell.classList.add("tr");
+        }else if(randomClass === 2){
+          cell.classList.add("bl");
+        }else if(randomClass === 3){
+          cell.classList.add("br");
+        }else if(randomClass === 4){
+          cell.classList.add("all");
+        }
+
+        randomClass = Math.random() * 4;
       }
     }
+
+    generateButton.addEventListener("click", generate);
+    
+    document.addEventListener("keydown", (event) => {
+      if(event.key === "Space"){
+        event.preventDefault();
+        generate();
+      }
+    });
+    
   });
 
   return (
@@ -35,6 +49,7 @@ function App() {
         <Row />
         <Row />
         <Row />
+        <button id="generateButton">Generate</button>
       </main> 
     </div>
   );
